81ae454c7ce087c7dc2143049c08ebfcdf81f24a
nong <nong@softinfo.com>
2021-10-08 09:26:38 +0700

    EP6.1

---
+++ lib/uitility/my_constant.dart
@@ -0,0 +1,11 @@
+
+class MyConstant {
+  // General
+  static String appName='Shopping Mall';
+  // Route
+  static String routeAuthen='/authen';
+  static String routeCreateAccount='/createAccount';
+  static String routeBuyerService='/buyerService';
+  static String routeSalerService='/salerService';
+  static String routeRiderService='/riderService';
+}
